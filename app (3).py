45fe88ac5c3cb2b854db0e261c3412d480cf39cc
Owais-arch <mohammedowaisbangi@gmail.com>
2026-03-16 12:06:31 +0530

    Add files via upload

---
+++ app (3).py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+rf = pickle.load(open("bangalore_house_model.pkl","rb"))
+
+st.title("Bangalore House Price Prediction")
+
+area_type = st.number_input("Area Type")
+availability = st.number_input("Availability")
+location = st.number_input("Location")
+society = st.number_input("Society")
+total_sqft = st.number_input("Total Sqft")
+bath = st.number_input("Bathrooms")
+balcony = st.number_input("Balconies")
+bhk = st.number_input("BHK")
+
+if st.button("Predict Price"):
+    
+    prediction = rf.predict([[area_type,availability,location,society,total_sqft,bath,balcony,bhk]])
+    
+    st.success(f"Estimated House Price: ₹ {prediction[0]} Lakhs")
